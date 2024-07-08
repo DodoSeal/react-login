c9eb8502ff0cd1aafc5fc8b0a0aacf95a5c11d3e
Max McDaniel <maxhmcd08@gmail.com>
2024-07-08 13:04:22 -0500

    Create basic account page with React

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
+function Login() {
+  return (
+    <div className="flex items-center place-content-center flex-wrap bg-slate-700 w-[25rem] aspect-[1/1.05] rounded-[1rem] p-4 [&>input]:aspect-[1/0.2] [&>input]:w-[20rem]">
+      <p className="text-white text-center font-semibold text-[2.25rem] mb-8">My Account</p>
+      <input className="border-none rounded-[1rem] focus:rounded-[0.25rem] p-[0.65rem] bg-slate-600 m-[0.5rem] text-white" type="text" placeholder="Email"></input>
+      <input className="border-none rounded-[1rem] focus:rounded-[0.25rem] p-[0.65rem] bg-slate-600 m-[0.5rem] text-white" type="password" placeholder="Password"></input>
+      <input className="border-none rounded-[1rem] hover:rounded-[0.25rem] p-[0.65rem] bg-blue-600 hover:bg-blue-500 m-[0.5rem] text-white focus:outline-none text-center text-3xl font-semibold cursor-pointer" type="button" value="Create Account"></input>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="w-screen h-screen p-0 m-0 bg-slate-900 flex items-center place-content-center focus:[&_*]:outline-none [&_*]:transition-all">
+      <Login />
     </div>
   );
 }
